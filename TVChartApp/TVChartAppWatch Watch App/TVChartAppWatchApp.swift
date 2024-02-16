f5fe550ad3122bbd2ccf2730d92dd85e43e2839f
Matthias Ferber <matthias.ferber@gmail.com>
2024-02-16 14:56:43 -0500

    Add watchOS target

---
+++ TVChartApp/TVChartAppWatch Watch App/TVChartAppWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVChartAppWatchApp.swift
+//  TVChartAppWatch Watch App
+//
+//  Created by Matthias Ferber on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TVChartAppWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
