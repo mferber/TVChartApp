b53ff0d7aaed2bb203a85a8990dcf48e85707898
Matthias Ferber <matthias.ferber@gmail.com>
2024-02-16 14:52:29 -0500

    Xcode iOS app skeleton (includes Quick/Nimble)

---
+++ TVChartApp/TVChartApp/TVChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVChartAppApp.swift
+//  TVChartApp
+//
+//  Created by Matthias Ferber on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TVChartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
