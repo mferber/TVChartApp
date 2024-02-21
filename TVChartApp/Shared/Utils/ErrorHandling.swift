8963c31c0a8fe0f9f82744d3bf315edcbde9fed5
Matthias Ferber <matthias.ferber@gmail.com>
2024-02-21 00:12:52 -0500

    Added general error type for misc purposes

--- TVChartApp/Shared/Utils/ErrorHandling.swift
+++ TVChartApp/Shared/Utils/ErrorHandling.swift
@@ -1,5 +1,15 @@
 import Foundation
 
+enum TVChartError: Error, CustomStringConvertible {
+  case general(_ msg: String?)
+
+  var description: String {
+    switch self {
+      case .general(let msg): return "General error \(msg == nil ? "" : ": \(msg!)")"
+    }
+  }
+}
+
 func handleError(_ error: Error, _ annotation: String? = nil) {
   print("Error: \(annotation == nil ? "" : "\(annotation!) - ")\(error)")
 }
